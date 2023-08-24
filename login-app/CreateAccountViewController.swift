e28ff94d503023906a5cfaf193a63183280af529
ganxsh <ganxshnalla@gmail.com>
2023-08-24 10:06:25 +0530

    added IBOutlets and Navigations

---
+++ login-app/CreateAccountViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CreateAccountViewController.swift
+//  login-app
+//
+//  Created by Ganesh Nalla on 24/08/23.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func SignupClicked(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
